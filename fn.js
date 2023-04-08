bc8257af57e8dd560cfd070a02b420669fb8827c
Mehmet Ersahin <ersahin.m@eplan.de>
2023-04-08 09:15:45 +0200

    mvp

---
+++ fn.js
@@ -0,0 +1,72 @@
+import axios from 'axios';
+import { DEBUG, db, plz, env } from './index.js';
+import fs from 'fs';
+import moment from 'moment/moment.js';
+
+export const saveDate = () => {
+    fs.writeFileSync('./db.json', JSON.stringify(db, null, 2));
+}
+
+export const telegram = function(message) {
+    var token = env.TELEGRAM_API_KEY;;
+    var chat_id = env.TELEGRAM_CHAT_ID;
+
+    console.log(message)
+    message = encodeURIComponent(message);
+    var url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${message}&parse_mode=HTML`;
+    // axios
+    axios.get(url).then((response) => {
+        console.log(response);
+    }).catch((error) => {
+        console.log(error);
+    });
+}
+
+export const getCoords = function (plz) {
+    let url = "https://nominatim.openstreetmap.org/search?postalcode=" + plz + "&format=json&addressdetails=1&limit=1&polygon_svg=1";
+    axios.get(url).then((response) => {
+        let lat = response.data[0].lat;
+        let lon = response.data[0].lon;
+        if (DEBUG)
+            console.log(lat, lon);
+        db[plz] = { "coords": { lat: lat, lon: lon } };
+        saveDate();
+        return { lat: lat, lon: lon };
+    }).then((coords) => {
+        // if not in db get locations
+        if (!db[plz].locations)
+            getLocations(coords);
+    }).catch((error) => {
+        console.log(error);
+    });
+}
+
+export const getLocations = function (coords) {
+    let url = "https://www.tuv.com/tos-pti-relaunch-2019/rest/ajax/getMergedLocationsByGeo";
+    let data = {
+        "locale": "de-DE",
+        "isLegacyTos": false,
+        "limit": 30,
+        "location": {
+            "latitude": coords.lat,
+            "longitude": coords.lon
+        },
+        "radius": 20,
+        "vehicleService": {
+            "id": 0
+        },
+        "vehicleType": {
+            "id": 44
+        }
+    };
+    axios.post(url, data).then((response) => {
+        if (DEBUG)
+            console.log(response.data);
+        db[plz] = { ...db[plz], "locations": response.data.vics };
+        saveDate();
+    }
+    ).catch((error) => {
+        console.log(error);
+    });
+}
+
---
+++ index.js
@@ -0,0 +1,123 @@
+import moment from 'moment/moment.js';
+import dotenv from 'dotenv';
+import fs from 'fs';
+import { getCoords, telegram } from './fn.js';
+import axios from 'axios';
+
+dotenv.config();
+// load .env.40789
+dotenv.config({ path: '.env.' + process.env.PLZ });
+//read db
+export let db = JSON.parse(fs.readFileSync('./db.json', 'utf8'));
+
+// read env vars
+export let env = process.env;
+
+// env var: DEBUG=true
+export let DEBUG=env.DEBUG == "1" ? true : false;
+export let TELEGRAM_API_KEY=env.TELEGRAM_API_KEY;
+export let addOneDay = env.TYPE == "incremental" ? true : false;
+export let custom_date = env.TYPE == "custom_date" ? true : false;
+export let plz = env.PLZ;
+
+// read db.json
+let date = moment().format('YYYY-MM-DD');
+if (env.START_DATE !== "") date = moment(START_DATE).format('YYYY-MM-DD');
+
+// db key object not found add
+if (!db[plz]) db[plz] = {};
+
+// if not in db get coords
+if (!db[plz].coords) await getCoords(plz);
+
+let new_date = getNewDate(date);
+
+// send every 1 second
+let interval = setInterval(async () => {
+    new_date = getNewDate(new_date);
+    await getAvailable(new_date);
+}, 1000);
+
+// fetch data axios
+// await getAvailable();
+
+//telegram start with env as message
+// env.40798 to string
+let env_json = dotenv.config({ path: '.env.' + plz }).parsed;
+// json to key value
+let env_str = "";
+for(let key in env_json) {
+    // telegram html new line
+    env_str += key + ": " + env_json[key] + "\n"
+}
+telegram(env_str)
+
+async function getAvailable(new_date) {
+    let res = await axios.post("https://www.tuv.com/tos-pti-relaunch-2019/rest/ajax/getVacanciesByDay", {
+        "locale": "de-DE",
+        "isLegacyTos": false,
+        "date": {
+            "date": new_date
+        },
+        "vehicleServices": [{
+            "id": 4007
+        }],
+        "vehicleType": {
+            "id": 44
+        },
+        "vics": db[plz].locations.map((location) => {
+            return {
+                "id": location.id,
+                "externalLocale": "de-DE",
+                "distance": 6
+            };
+        })
+    });
+
+    // console.log(res.data);
+    //db[plz].last = res.data;
+    //saveDate();
+    //get available time slots
+    let bulundu = false;
+    var times = res.data;
+    for (const ort of times.timetables) {
+        // if (DEBUG) console.log('ort => ', ort);
+        for (const time of ort.timeRangeVacancies) {
+            // if (DEBUG) console.log('time => ', time);
+            if (time.hasTimes) {
+                // console.log(ort);
+                bulundu = true;
+            }
+            for (const slot of time.timeslots) {
+                // if (DEBUG) console.log('slot => ', slot);
+                if (slot.availableDates.length) {
+                    let msg = `${slot.availableDates.join()} - ${date} - ${orts[ort.vic.id]}`;
+                    telegram(msg);
+                    // console.log(`${orts[time.vic.id]}`)
+                }
+            }
+        }
+    }
+}
+
+function  getNewDate(date) {
+    let new_date = date;
+    switch (env.TYPE) {
+        case "incremental":
+            if (addOneDay) new_date = moment(new_date).add(1, 'days').format('YYYY-MM-DD');
+            if(new_date > env.END_DATE) new_date = env.START_DATE === "" ? moment().format('YYYY-MM-DD') : env.START_DATE;
+            break;
+        case "custom_date":
+            // get random date custom_date
+            let custom_dates = env.CUSTOM_DATES.split(',');
+            new_date = custom_dates[Math.floor(Math.random() * custom_dates.length)];
+            break;
+        default:
+            new_date = date;
+            break;
+    }
+
+    if (DEBUG) console.log(new_date)
+    return new_date;
+}
+
