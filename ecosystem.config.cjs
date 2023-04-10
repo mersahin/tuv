a6a6bef9d9e2bb7d29f90eda332e70e513fd5244
Mehmet Ersahin <m@ersah.in>
2023-04-11 01:41:40 +0200

    eco fix #3

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -105,7 +105,7 @@ module.exports = {
       repo : 'https://github.com/mersahin/tuv',
       path : '/root/tuv',
       'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.cjs',
+      'post-deploy' : 'npm install && pm2 reload /root/tuv/ecosystem.config.cjs',
       'pre-setup': ''
     }
   },   
